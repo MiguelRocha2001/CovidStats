a84d986266c07bcb13d1f77ca1762d7cc2b9333e
Miguel Rocha <migasrocha1@hotmail.com>
2022-01-25 01:16:36 +0000

    At this point, the app shows some stats about covid cases, in Portugal, since the start of the outbreak

--- app/src/main/java/app/covidstats/MainActivity.kt
+++ app/src/main/java/app/covidstats/MainActivity.kt
@@ -3,25 +3,43 @@ package app.covidstats
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.covidstats.ui.DataProvider
 import app.covidstats.ui.Model
 
+// TODO make composable to format covid results
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val results by remember { mutableStateOf(Model()) }
+            val covidStats by remember { mutableStateOf(Model()) }
             Column() {
-                Button(onClick = { results.getCovidCases() }) {
+                Button(onClick = { covidStats.getCovidCases() }) {
                     Text(text = "Show Results", fontSize = 60.sp)
                 }
-                Text(text = results.results ?: "empty", fontSize = 20.sp)
+                ShowCovidStats(covidStats.results)
             }
         }
     }
 }
 
+@Composable
+private fun ShowCovidStats(covidStats: DataProvider?) {
+    Column() {
+        val stats = covidStats
+        stats?.dataProvider?.forEach { covidStats ->
+            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.padding(20.dp)) {
+                Text(text = "date_stamp: ${covidStats.date_stamp}", fontSize = 20.sp)
+                Text(text = "cnt_confirmed: ${covidStats.cnt_confirmed}", fontSize = 20.sp)
+                Text(text = "cnt_death: ${covidStats.cnt_death}", fontSize = 20.sp)
+                Text(text = "cnt_recovered: ${covidStats.cnt_recovered}", fontSize = 20.sp)
+            }
+        }
+    }
+}
 
--- app/src/main/java/app/covidstats/ui/Uri.kt
+++ app/src/main/java/app/covidstats/ui/Uri.kt
@@ -12,25 +12,24 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 @Serializable
+data class DataProvider(val dataProvider: Array<Data>)
+
+@Serializable
 data class Data(val date_stamp: String, val cnt_confirmed: Int, val cnt_death: Int, val cnt_recovered: Int)
 
 const val CASES_URI = "https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_confirmed,cnt_death,cnt_recovered&where=(iso3166_1=PT)&format=amcharts&limit=5"
 
-@Serializable
-data class DataProvider(val dataProvider: Array<Data>)
-
 class Model() {
-    var results by mutableStateOf<String?>(null)
+    var results by mutableStateOf<DataProvider?>(null)
 
     fun getCovidCases() {
         Fuel.get(CASES_URI)
             .response { request, response, result ->
                 // to show body response
                 //Log.v("response", result.get().decodeToString())
-                val obj = Json.decodeFromString<DataProvider>(result.get().decodeToString())
+                results = Json.decodeFromString<DataProvider>(result.get().decodeToString())
                 // to show json object with response
                 //Log.v("response", obj.toString())
-                results = obj.toString()
             }
     }
 }
