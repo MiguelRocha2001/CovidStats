6367567be8213232bd6070da85b4ec80114bb716
Miguel Rocha <migasrocha1@hotmail.com>
2022-01-27 23:30:17 +0000

    The app shows world covid stats

--- app/src/main/java/app/covidstats/MainActivity.kt
+++ app/src/main/java/app/covidstats/MainActivity.kt
@@ -3,14 +3,14 @@ package app.covidstats
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.sp
-import app.covidstats.uris.DataProvider
+import app.covidstats.Ui.ShowWorldCovidStats
 import app.covidstats.uris.Model
 
 // TODO make composable to format covid results
@@ -19,33 +19,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val covidStats by remember { mutableStateOf(Model()) }
+
             Column() {
                 Button(onClick = { covidStats.getCovidCases() }) {
                     Text(text = "Show Results", fontSize = 60.sp)
                 }
-                ShowCovidStats(covidStats.results)
-            }
-        }
-    }
-}
-
-@Composable
-private fun ShowCovidStats(covidStats: DataProvider?) {
-    LazyColumn() {
-        val stats = covidStats
-        stats?.dataProvider?.forEach { covidStats ->
-            item {
-                Column(
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .padding(20.dp)
-                ) {
-                    Text(text = "date: ${covidStats.date_stamp}", fontSize = 20.sp)
-                    Text(text = "country: ${covidStats.iso3166_1}", fontSize = 20.sp)
-                    Text(text = "cases: ${covidStats.cnt_confirmed}", fontSize = 20.sp)
-                    //Text(text = "deaths: ${covidStats.cnt_death}", fontSize = 20.sp)
-                    //Text(text = "recovered: ${covidStats.cnt_recovered}", fontSize = 20.sp)
-                }
+                ShowWorldCovidStats(covidStats.results)
             }
         }
     }
--- app/src/main/java/app/covidstats/uris/Uri.kt
+++ app/src/main/java/app/covidstats/uris/Uri.kt
@@ -9,33 +9,52 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
-@Serializable
-data class DataProvider(val dataProvider: Array<Data2>)
+// TODO use this API: https://disease.sh/docs/?urls.primaryName=version%203.0.0
+
+const val WORLD_CASES_URI = "https://disease.sh/v3/covid-19/all"
 
 /**
  * Represents a JSON object that holds data for Portugal covid stats
  */
 @Serializable
-data class Data1(val date_stamp: String, val cnt_confirmed: Int, val cnt_death: Int, val cnt_recovered: Int)
+data class Data(val date_stamp: String, val cnt_confirmed: Int, val cnt_death: Int, val cnt_recovered: Int)
 
 /**
  * Represents a JSON object that holds data for world covid stats
  */
 @Serializable
-data class Data2(val date_stamp: String, val iso3166_1: String, val cnt_confirmed: Int)
-
-const val CASES_URI = "https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_confirmed,cnt_death,cnt_recovered&where=(iso3166_1=PT)&format=amcharts&limit=10"
-const val WORLD_CASES_URI = "https://covid19.richdataservices.com/rds/api/query/int/jhu_country/tabulate?dims=date_stamp,iso3166_1&where=(year_stamp=2020) AND (iso3166_1=US OR iso3166_1=CA OR iso3166_1=ES OR iso3166_1=IT OR iso3166_1=CN)&measure=cnt_confirmed:SUM(cnt_confirmed)&inject=true&metadata=true&orderBy=date_stamp ASC,iso3166_1 ASC&totals=true&format=amcharts"
+data class WorldData(val updated: Long,
+                     val cases: Long,
+                     val todayCases: Long,
+                     val deaths: Long,
+                     val todayDeaths: Long,
+                     val recovered: Long,
+                     val todayRecovered: Long,
+                     val active: Long,
+                     val critical: Long,
+                     val casesPerOneMillion: Long,
+                     val deathsPerOneMillion: Double,
+                     val tests: Long,
+                     val testsPerOneMillion: Double,
+                     val population: Long,
+                     val oneCasePerPeople: Long,
+                     val oneDeathPerPeople: Long,
+                     val oneTestPerPeople: Long,
+                     val activePerOneMillion: Double,
+                     val recoveredPerOneMillion: Double,
+                     val criticalPerOneMillion: Double,
+                     val affectedCountries: Long
+)
 
 class Model() {
-    var results by mutableStateOf<DataProvider?>(null)
+    var results by mutableStateOf<WorldData?>(null)
 
     fun getCovidCases() {
         Fuel.get(WORLD_CASES_URI)
             .response { request, response, result ->
                 // to show body response
                 //Log.v("response", result.get().decodeToString())
-                results = Json.decodeFromString<DataProvider>(result.get().decodeToString())
+                results = Json.decodeFromString<WorldData>(result.get().decodeToString())
                 // to show json object with response
                 //Log.v("response", obj.toString())
             }
