42784c2412e86964f8430c15a38ef3618bab914c
Miguel Rocha <migasrocha1@hotmail.com>
2022-02-01 21:48:34 +0000

    Setting up images for the continents

--- app/src/main/java/app/covidstats/Ui/WindowContent.kt
+++ app/src/main/java/app/covidstats/Ui/WindowContent.kt
@@ -1,20 +1,24 @@
 package app.covidstats.Ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
+import app.covidstats.R
 import app.covidstats.model.Model
 
 @Composable
 fun MainWindow() {
     val covidStats = remember { Model() }
     var showMenu by remember { mutableStateOf(false) }
+    var showContinentFlags by remember { mutableStateOf(false) }
     var option by remember { mutableStateOf<(() -> Unit)?>(null) }
     Column(modifier = Modifier.fillMaxWidth()) {
         Button(
@@ -36,12 +40,31 @@ fun MainWindow() {
                 showMenu = false
             }
             OptionView("Show Continent Covid Stats") {
-                covidStats.getContinentCovidStats("Europe")
+                covidStats.emptyResults()
+                showContinentFlags = true
                 showMenu = false
             }
             OptionView("Exit") {
                 covidStats.emptyResults()
                 showMenu = false
+            }
+        }
+        if (showContinentFlags) {
+            Button(onClick = {
+                showContinentFlags = false
+                covidStats.getContinentCovidStats("Europe")
+            }) {
+                val image: Painter = painterResource(id = R.drawable.european_union_flag)
+                Row(Modifier.fillMaxWidth()) {
+                    Image(
+                        modifier = Modifier.weight(1f, fill = false)
+                            .aspectRatio(image.intrinsicSize.width / image.intrinsicSize.height)
+                            .fillMaxWidth(),
+                        contentScale = ContentScale.Fit,
+                        painter = image,
+                        contentDescription = "European Union Flag",
+                    )
+                }
             }
         }
         ShowWorldCovidStats(covidStats.results)
