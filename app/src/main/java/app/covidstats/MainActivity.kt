e61e6b114f37e8edb3825bd1502c07cc3150c3be
Miguel <migasrocha1@hotmail.com>
2022-04-25 23:44:19 +0100

    Trying to customize toolbar

--- app/src/main/java/app/covidstats/MainActivity.kt
+++ app/src/main/java/app/covidstats/MainActivity.kt
@@ -1,22 +1,18 @@
 package app.covidstats
 
+import android.R
 import android.os.Bundle
-import android.view.WindowManager
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.graphics.Color
 import app.covidstats.ui.MainWindow
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO -> uncomment this line to format search bar
-        /*
-        window.setFlags(
-            WindowManager.LayoutParams.TYPE_STATUS_BAR,
-            WindowManager.LayoutParams.TYPE_STATUS_BAR
-        )
-
-         */
         setContent {
             MainWindow(this)
         }
